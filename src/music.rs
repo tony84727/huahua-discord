18d7eaad819a467663dfa4bb50deb71911c6a14e
Tony Duan <tony84727@gmail.com>
2022-03-16 22:33:08 +0800

    feat: join if not in the channel when play

--- src/music.rs
+++ src/music.rs
@@ -7,47 +7,86 @@ use serenity::{
         id::{ChannelId, GuildId},
     },
 };
-#[command]
-pub async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    match args.single::<String>() {
-        Ok(channel_id) => {
-            let channel_id = match ChannelId::from_str(ctx, &channel_id).await {
-                Ok(id) => id,
-                Err(_) => {
-                    return Ok(());
-                }
-            };
-            let channel = match channel_id.to_channel(ctx).await {
-                Ok(channel) => channel,
-                Err(_) => {
-                    return Ok(());
-                }
-            };
-            match channel {
-                Channel::Guild(channel) if channel.kind == ChannelType::Voice => channel,
-                _ => {
-                    return Ok(());
-                }
-            };
-            if let Err(err) = join_channel(ctx, msg.guild_id.unwrap(), channel_id).await {
-                log::error!("fail to join voice channel, {:?}", err);
-            }
-            return Ok(());
+async fn try_join_channel(ctx: &Context, msg: &Message, channel_id: Option<ChannelId>) {
+    match channel_id {
+        Some(channel_id) => {
+            join_channel(ctx, msg.guild_id.unwrap(), channel_id)
+                .await
+                .unwrap();
         }
-        Err(_) => (),
-    }
-    match find_voice_channel_of_user(ctx, msg).await {
-        Some((guild_id, channel_id)) => match join_channel(ctx, guild_id, channel_id).await {
-            Ok(()) => (),
-            Err(err) => {
-                log::error!("fail to join voice channel, {:?}", err)
+        None => match find_voice_channel_of_user(ctx, msg).await {
+            Some((guild_id, channel_id)) => match join_channel(ctx, guild_id, channel_id).await {
+                Ok(()) => (),
+                Err(err) => {
+                    log::error!("fail to join voice channel, {:?}", err)
+                }
+            },
+            None => {
+                check_msg(msg.reply(ctx, format!("您沒有在任何語音頻道")).await);
             }
         },
-        None => {
-            msg.reply(ctx, format!("您沒有在任何語音頻道")).await?;
+    };
+}
+
+async fn try_parse_voice_channel_id(ctx: &Context, id: &str) -> Option<ChannelId> {
+    let channel_id = match ChannelId::from_str(ctx, &id).await {
+        Ok(id) => id,
+        Err(_) => {
+            return None;
+        }
+    };
+    let channel = match channel_id.to_channel(ctx).await {
+        Ok(channel) => channel,
+        Err(_) => {
+            return None;
+        }
+    };
+    match channel {
+        Channel::Guild(channel) if channel.kind == ChannelType::Voice => Some(channel_id),
+        _ => {
+            return None;
         }
     }
-    CommandResult::Ok(())
+}
+
+#[command]
+pub async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let channel_id = args.single::<String>().ok();
+    let channel_id = match channel_id {
+        Some(id) => try_parse_voice_channel_id(ctx, &id).await,
+        None => None,
+    };
+    try_join_channel(ctx, msg, channel_id).await;
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+enum PlayError {
+    NotInChannel,
+}
+async fn try_play_ytdl(
+    ctx: &Context,
+    msg: &Message,
+    url: &str,
+    guild_id: GuildId,
+) -> Result<(), PlayError> {
+    let manager = songbird::get(ctx).await.unwrap();
+    match manager.get(guild_id) {
+        Some(handler_lock) => {
+            let mut handler = handler_lock.lock().await;
+            let source = match songbird::ytdl(&url).await {
+                Ok(source) => source,
+                Err(why) => {
+                    log::error!("cannot play youtube, url: {:?}", why);
+                    check_msg(msg.reply(&ctx.http, "無法播放QAQ").await);
+                    return Ok(());
+                }
+            };
+            handler.play_only_source(source);
+            Ok(())
+        }
+        None => Err(PlayError::NotInChannel),
+    }
 }
 
 #[command]
@@ -61,22 +100,15 @@ pub async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     };
     let guild = msg.guild(&ctx.cache).await.unwrap();
     let guild_id = guild.id;
-    let manager = songbird::get(ctx).await.unwrap();
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
-        let source = match songbird::ytdl(&url).await {
-            Ok(source) => source,
-            Err(why) => {
-                log::error!("cannot play youtube, url: {:?}", why);
-                check_msg(msg.reply(&ctx.http, "無法播放QAQ").await);
-                return Ok(());
-            }
-        };
-        handler.play_only_source(source);
-        Ok(())
-    } else {
-        join(ctx, msg, args).await
+    let play_result = try_play_ytdl(ctx, msg, &url, guild_id).await;
+    match play_result {
+        Err(err) if err == PlayError::NotInChannel => {
+            try_join_channel(ctx, msg, None).await;
+            try_play_ytdl(ctx, msg, &url, guild_id).await.unwrap();
+        }
+        _ => (),
     }
+    Ok(())
 }
 
 #[command]
